5f2412b0cce79918c923644c949d3a2c2ff3553d
Jordi <jvelascomo@uoc.edu>
2024-01-10 19:52:03 +0100

    final version v1

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { Observable} from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -14,9 +14,6 @@ export class UserService  {
 
   private user: any[] = [];
   private usersUrl = 'assets/users.json';
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
 
   constructor(private http: HttpClient) { }
 
